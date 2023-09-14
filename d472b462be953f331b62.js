104dbe284ae0d0bdccb332cc2a5eea36bd223cdb
RenaDesu <101468409+RenaDesu@users.noreply.github.com>
2023-09-14 16:11:55 +0000

    Deploying to gh-pages from @ RenaDesu/calorie_tracker@1dd198503824254a454439da4df7ea32d644e7ce 🚀

---
+++ d472b462be953f331b62.js
@@ -0,0 +1,1 @@
+import"../index.html";import"../assets/scss/style.scss";import"./modules/set-limit";import"./modules/localstorage";import"./modules/add-current-table";import"./modules/add-total-table";import"./modules/pie-chart";import"./modules/filter";import"./modules/utils";
